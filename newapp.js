6af225229b287c6b1e0770510054e7116004fb6c
mmdrza <mmdrezaabed9@gmail.com>
2025-11-21 14:59:27 +0330

    v1

--- newapp.js
+++ newapp.js
@@ -25,12 +25,12 @@ if (!localStorage.getItem("sound")) {
 function renderTheme() {
     if (localStorage.getItem("theme")) {
         if (localStorage.getItem("theme") == "light") {
-            themeLink.href = "css/light.css";
+            themeLink.href = "css/ltheme.css";
             changeThemeBtn.innerHTML =
                 'Dark theme<svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000"><path d="M479.96-144Q340-144 242-242t-98-238q0-140 97.93-238t237.83-98q13.06 0 25.65 1 12.59 1 25.59 3-39 29-62 72t-23 92q0 85 58.5 143.5T648-446q49 0 92-23t72-62q2 13 3 25.59t1 25.65q0 139.9-98.04 237.83t-238 97.93Zm.04-72q82 0 148.78-47.07Q695.55-310.15 727-386q-20 5-39.67 8.5Q667.67-374 648-374q-113.86 0-193.93-80.07Q374-534.14 374-648q0-19.67 3.5-39.33Q381-707 386-727q-75.85 31.45-122.93 98.22Q216-562 216-480q0 110 77 187t187 77Zm-14-250Z"/></svg>';
             svgColor("#000");
         } else {
-            themeLink.href = "css/dark.css";
+            themeLink.href = "css/dtheme.css";
             changeThemeBtn.innerHTML =
                 'Light theme<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#fff"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm-.23 72Q400-288 344-344.23q-56-56.22-56-136Q288-560 344.23-616q56.22-56 136-56Q560-672 616-615.77q56 56.22 56 136Q672-400 615.77-344q-56.22 56-136 56ZM216-444H48v-72h168v72Zm696 0H744v-72h168v72ZM444-744v-168h72v168h-72Zm0 696v-168h72v168h-72ZM269-642 166-742l51-55 102 104-50 51Zm474 475L642-268l49-51 103 101-51 51ZM640-691l102-101 51 49-100 103-53-51ZM163-217l105-99 49 47-98 104-56-52Zm317-263Z"/></svg>';
             svgColor("#fff");
